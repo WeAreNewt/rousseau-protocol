ccdfd995637eaa3c19c1f042047336c9accf9597
JosepBove <josep@aave.com>
2022-08-12 14:14:30 +0100

    Structure and license

---
+++ README.md
@@ -0,0 +1,3 @@
+# Avara Protocol
+
+Please read: https://www.notion.so/aave/Avara-Protocol-42603f50e36f4e1abec1c1a6d5749a60
--- test/AvaraProtocol.t.sol
+++ test/AvaraProtocol.t.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-contract ContractTest is Test {
+contract AvaraProtocolTests is Test {
     function setUp() public {}
 
     function testExample() public {
