628b01338103139a2b7dd23c0de1145949734b1a
JosepBove <josep@aave.com>
2022-11-07 09:44:47 +0000

    Change SVG

--- frontend/src/components/ElegibilityBanner.tsx
+++ frontend/src/components/ElegibilityBanner.tsx
@@ -1,9 +1,11 @@
 const EligibilityBanner: React.FC = () => {
   return (
-    <div className="flex flex-row w-2/3 h-[65px] bg-gray-800 text-[#FFFFFF] justify-center items-center">
-      your wallet is not eligible to participate in the voting of these values
-    </div>
-  )
+      <>
+          <div className="flex flex-row w-2/3 h-[65px] bg-gray-800 text-[#FFFFFF] justify-center items-center">
+              your wallet is not eligible to participate in the voting of these values
+          </div>
+      </>
+  );
 }
 
 export default EligibilityBanner
