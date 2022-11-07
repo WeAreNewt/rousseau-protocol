a8af3c07dcbf96dfe1dae32406ca077a28518923
JosepBove <josep@aave.com>
2022-11-07 14:47:20 +0000

    Add modal basics

--- frontend/src/components/modals/AddModal.tsx
+++ frontend/src/components/modals/AddModal.tsx
@@ -1,16 +1,44 @@
-export const AddModal: React.FC = () => {
+import { useState } from "react";
+
+type AddModalProps = {
+  show: boolean;
+  setShow: (show: boolean) => void;
+};
+
+export const AddModal: React.FC<AddModalProps> = ({
+  show,
+  setShow,
+}: AddModalProps) => {
   return (
-    <div className="rounded-lg shadow-2xl items-center justify-center border-gray-1000 border">
-      <p>
-        propose adding value
-      </p>
-      <textarea className="box-border rounded-lg border-solid border-gray-1000 border w-{480} h-{432} left-6 top-16 absolute resize focus:outline-none text-gray-600">your proposal text here</textarea>
-      <button
-        className="justify-center items-center bg-gray-600 text-white rounded-full w-56 h-10 mt-4"
-        type="button"
+    <div
+      onMouseDown={() => setShow(false)}
+      className={`${
+        show ? "block" : "hidden"
+      } fixed h-screen w-screen z-20 flex items-center justify-center create-btn-gradient-transparent px-4 lg:px-0 top-0 left-0`}
+    >
+      <div
+        onMouseDown={(e) => e.stopPropagation()}
+        className={
+          "flex flex-col items-center w-[530px] h-[432px] rounded-[10px]"
+        }
+        style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
       >
-        propose change
-      </button>
+        <div className="flex flex-col w-full px-5 mt-6">
+          <b className="flex flex-row font-medium text-base leading-6">
+            propose adding value
+          </b>
+          <textarea
+            className="flex flex-row box-border border rounded-lg border-solid border-gray-1000 resize-none focus:outline-none text-gray-600 px-py mt-4 h-[300px] left-[23px] top-[70px] px-[10px] py-[10px]"
+            defaultValue={"your proposal text here"}
+          />
+          <button
+            className=" flex flex-row bg-gray-600 text-white rounded-full w-56 h-10 justify-center items-center mt-2 ml-auto"
+            type="button"
+          >
+            propose add
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/modals/ChangeModal.tsx
+++ frontend/src/components/modals/ChangeModal.tsx
@@ -1,53 +1,69 @@
-export const ChangeModal: React.FC = () => {
+type AddModalProps = {
+  show: boolean;
+  setShow: (show: boolean) => void;
+};
+
+export const ChangeModal: React.FC<AddModalProps> = ({
+  show,
+  setShow,
+}: AddModalProps) => {
   return (
     <div
-      className="flex flex-col items-center w-[530px] h-[432px] rounded-[10px]"
-      style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+      onMouseDown={() => setShow(false)}
+      className={`${
+        show ? "block" : "hidden"
+      } fixed h-screen w-screen z-20 flex items-center justify-center create-btn-gradient-transparent px-4 lg:px-0 top-0 left-0`}
     >
-      <button
-        className="flex flex-row justify-center items-center bg-gray-600 text-white rounded-full w-56 h-10 mt-4"
-        type="button"
+      <div
+        className="flex flex-col items-center w-[530px] h-[432px] rounded-[10px]"
+        onMouseDown={(e) => e.stopPropagation()}
+        style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
       >
-        propose removal
-      </button>
-      <div className="mt-5 mb-5">
-        <div className="flex flex-row items-center">
-          <svg
-            width="216"
-            height="1"
-            viewBox="0 0 216 1"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="flex flex-col mr-4"
-          >
-            <line y1="0.5" x2="215.142" y2="0.5" stroke="#838383" />
-          </svg>
-          or
-          <svg
-            width="216"
-            height="1"
-            viewBox="0 0 216 1"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="flex flex-col ml-4"
-          >
-            <line y1="0.5" x2="215.142" y2="0.5" stroke="#838383" />
-          </svg>
-        </div>
-      </div>
-      <div className="flex flex-col w-full px-5">
-        <p className="flex flex-row font-medium text-base leading-6">
-          propose change
-        </p>
-        <textarea className="flex flex-row box-border border rounded-lg border-solid border-gray-1000 resize-none focus:outline-none text-gray-600 px-py mt-4 h-[200px] left-[23px] top-[186px] px-[10px] py-[10px]">
-          your proposal text here
-        </textarea>
         <button
-          className=" flex flex-row bg-gray-600 text-white rounded-full w-56 h-10 justify-center items-center mt-2 ml-auto"
+          className="flex flex-row justify-center items-center bg-gray-600 text-white rounded-full w-56 h-10 mt-4"
           type="button"
         >
-          propose change
+          propose removal
         </button>
+        <div className="mt-5 mb-5">
+          <div className="flex flex-row items-center">
+            <svg
+              width="216"
+              height="1"
+              viewBox="0 0 216 1"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="flex flex-col mr-4"
+            >
+              <line y1="0.5" x2="215.142" y2="0.5" stroke="#838383" />
+            </svg>
+            or
+            <svg
+              width="216"
+              height="1"
+              viewBox="0 0 216 1"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="flex flex-col ml-4"
+            >
+              <line y1="0.5" x2="215.142" y2="0.5" stroke="#838383" />
+            </svg>
+          </div>
+        </div>
+        <div className="flex flex-col w-full px-5">
+          <b className="flex flex-row font-medium text-base leading-6">
+            propose change
+          </b>
+          <textarea className="flex flex-row box-border border rounded-lg border-solid border-gray-1000 resize-none focus:outline-none text-gray-600 px-py mt-4 h-[200px] left-[23px] top-[186px] px-[10px] py-[10px]">
+            your proposal text here
+          </textarea>
+          <button
+            className=" flex flex-row bg-gray-600 text-white rounded-full w-56 h-10 justify-center items-center mt-2 ml-auto"
+            type="button"
+          >
+            propose change
+          </button>
+        </div>
       </div>
     </div>
   );
